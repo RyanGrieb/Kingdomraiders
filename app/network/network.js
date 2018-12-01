2e73e06652811ab687ca998f10dfdab70cd6dc54
rhin <ryanrhin@gmail.com>
2018-12-01 12:55:02 -0600

    Adding ping.

--- app/network/network.js
+++ app/network/network.js
@@ -6,6 +6,7 @@ import PlayerListener from "./listeners/player/playerlistener";
 import MenuScreen from "../ui/scene/menuscreen";
 import MonsterListener from "./listeners/entity/monsterlistener";
 import LoadingScreen from "../ui/scene/loadingscreen";
+import Latencey from "./latencey";
 
 export default class Network {
     constructor() {
@@ -15,13 +16,14 @@ export default class Network {
         this.listeners = [];
 
         this.listeners.push(new PlayerListener(), new AccountListener(), new MapListener(), new MonsterListener());
+
+        this.latencey = new Latencey();
     }
 
     connect() {
 
         this.ws = new WebSocket("ws://45.31.152.207:5000");
         //this.ws = new WebSocket("ws://localhost:5000");
-
 
         this.ws.onopen = () => this.onConnection();
 
@@ -40,12 +42,21 @@ export default class Network {
     }
 
     onMessage(message) {
+        if (message.data.includes("pong")) {
+            this.latencey.recievePong(message.data);
+            return;
+        }
+
+
         //Allow our listeners to handle the message seperately
         for (var i = 0; i < this.listeners.length; i++) {
             this.listeners[i].handleMessage(message);
         }
     }
 
+    lerp(v0, v1, t) {
+        return v0 * (1 - t) + v1 * t
+    }
 
     sendMessage(message) {
         this.ws.send(message);
